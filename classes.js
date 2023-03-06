7204b524e9e89a0081f040b9c0d551a384f659ef
leon <leontieber@gmx.net>
2023-03-06 13:03:50 +0100

    added basic function

---
+++ classes.js
@@ -0,0 +1,76 @@
+class Sprite {
+    constructor({
+        position,
+        image,
+        frames= {max: 1, hold: 10},
+        animate = false,
+        sprites
+    }){
+        this.position = position
+        this.image = image
+        this.frames = {...frames, val:0, elapsed:0}
+        this.animate = animate
+        this.sprites = sprites
+        this.image.onload = () => {
+            this.width = this.image.width /this.frames.max
+            this.height = this.image.height
+        }
+    
+    }
+    draw() {
+        c.save()
+        c.drawImage(
+            this.image,
+            this.frames.val * this.width,
+            0,
+            this.image.width / this.frames.max,
+            this.image.height,
+            this.position.x,
+            this.position.y,
+            this.image.width / this.frames.max,
+            this.image.height
+            );
+            c.restore()
+            if (!this.animate) return
+
+            if (this.frames.max > 1) {
+                this.frames.elapsed++
+            }
+            if (this.frames.elapsed % this.frames.hold === 0) {
+            if (this.frames.val < this.frames.max - 1) this.frames.val++
+            else this.frames.val = 0
+    }
+}
+    enmeyAI(){
+        let distX = player.position.x - this.position.x;
+        let distY = player.position.y - this.position.y;
+        if(distX < 0) {
+            this.position.x -= 1.5;
+        }else this. position.x += 1.5;
+        if(distY < 0) {
+            this.position.y -= 1.5;
+        }else this.position.y += 1.5;
+    }
+}
+
+class WeaponMelee {
+    constructor({
+        position,
+        slot,
+        damage,
+        attackSpeed,
+        image
+    }){
+    this.position = position
+    this.slot = slot
+    this.damage = damage
+    this.attackSpeed = attackSpeed
+    this.image = image
+    }
+    draw(){
+        c.drawImage(this.image, this.position.x, this.position.y);
+    }
+    attack(){
+
+    }
+}
--- main.js
+++ main.js
@@ -2,7 +2,6 @@ const canvas = document.querySelector("canvas");
 const c = canvas.getContext("2d");
 canvas.width = 1024;
 canvas.height = 576;
-console.log(c);
 
 c.fillRect(0,0, canvas.width, canvas.height);
 
@@ -23,54 +22,12 @@ playerImageLeft.src = "./images/test_character/playerLeft.png"
 const playerImageRight = new Image();
 playerImageRight.src = "./images/test_character/playerRight.png"
 
-class Sprite {
-    constructor({
-        position,
-        image,
-        frames= {max: 1, hold: 10},
-        animate = false,
-        sprites
-    }){
-        this.position = position
-        this.image = image
-        this.frames = {...frames, val:0, elapsed:0}
-        this.animate = animate
-        this.sprites = sprites
-        this.image.onload = () => {
-            this.width = this.image.width /this.frames.max
-            this.height = this.image.height
-        }
-    
-    }
-    draw() {
-        c.save()
-        c.drawImage(
-            this.image,
-            this.frames.val * this.width,
-            0,
-            this.image.width / this.frames.max,
-            this.image.height,
-            this.position.x,
-            this.position.y,
-            this.image.width / this.frames.max,
-            this.image.height
-            );
-            c.restore()
-            if (!this.animate) return
 
-            if (this.frames.max > 1) {
-                this.frames.elapsed++
-            }
-            if (this.frames.elapsed % this.frames.hold === 0) {
-            if (this.frames.val < this.frames.max - 1) this.frames.val++
-            else this.frames.val = 0
-    }
-}
-}
+const embyImageFront = new Image();
+embyImageFront.src = "./images/enemies/EmbyFront.png";
 
-
-
-
+const embyImageSprite = new Image();
+embyImageSprite.src = "./images/enemies/EmbySprite.png";
 
 const background = new Sprite({
     position: {
@@ -98,6 +55,36 @@ const player = new Sprite({
         right: playerImageRight,
         left: playerImageLeft
     }
+})
+
+const emby = new Sprite({
+    position:{
+        x:+100,
+        y:+70
+    },
+    sprites: {
+        up:embyImageSprite,
+        down:embyImageFront,
+        left:embyImageFront,
+        right:embyImageSprite
+    },
+    image:embyImageFront,
+    frames: {
+        max:4,
+        hold:8
+    }
+})
+
+const swordImage = new Image();
+swordImage.src = "./assets/sword.webp";
+
+const sword = new WeaponMelee({
+    image: swordImage,
+    position: {
+        x: player.position.x + 20,
+        y: player.position.y + 20
+    },
+    damage: 20
 })
 
 
@@ -151,35 +138,41 @@ addEventListener("keyup", (event) => {
 })
 
 
+
+
 const animate = () => {
     window.requestAnimationFrame(animate);
     background.draw();
     player.draw();
     if (keys.w.pressed) {
-        background.position.y += 3;
+        player.position.y -= 3;
         player.image = player.sprites.up;
         player.animate = true;
+        
     }
 
     else if (keys.s.pressed) {
-        background.position.y -= 3;
+        player.position.y += 3;
         player.image = player.sprites.down;
         player.animate = true;
     }
 
     else if (keys.a.pressed) {
-        background.position.x += 4;
+        player.position.x -= 3;
         player.image = player.sprites.left;
         player.animate = true;
     }
 
     else if (keys.d.pressed) {
-        background.position.x -= 4;
+        player.position.x += 3;
         player.image = player.sprites.right;
         player.animate = true;
 
     }else  {player.animate = false;}
 
+    emby.draw()
+    emby.enmeyAI()
+    sword.draw()
     
 }
 
